afab1b62ef8eb80b42da47740b13c5e983cb1e64
Nicolas De Arrascaeta <nicolasgim.99@gmail.com>
2023-04-14 20:31:04 -0300

    BaSP-W05-2023: Variables and Operators.

---
+++ Week-05/Exercises/variables-and-operators.js
@@ -0,0 +1,29 @@
+/*Variables y Operadores*/
+
+/*A*/
+
+var num1,num2,res;
+num1=12;
+num2=12;
+res=num1+num2;
+console.log("La suma de los numeros es:"+res);
+
+
+/*B*/
+
+const nam="Pepito";
+const last_Name=" Peréz";
+const data=nam+last_Name;
+console.log("Su nombre y apellido son: "+data);
+
+
+/*C*/
+
+const txt1="Ya no se que hacer";
+const txt2="Estoy aburrido";
+var lenght=txt1.length+txt2.length;
+console.log("El largo de la suma de las palabras es: "+lenght);
+
+
+
+
